ff776ab4b541fcfb45f08060d201ea7557237ad6
Brian Park <yaru22@gmail.com>
2013-11-17 16:09:23 -0500

    Solved Http Server problem in stream-adventure.

---
+++ stream-adventure/httpServer.js
@@ -0,0 +1,11 @@
+var http    = require('http');
+var through = require('through');
+
+var server = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    req.pipe(through(function (buf) {
+      this.queue(buf.toString().toUpperCase());
+    })).pipe(res);
+  }
+});
+server.listen(process.argv[2]);
