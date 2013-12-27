af9c15c9f9ada6dece9c657d746f70c02c81d651
Brian Park <yaru22@gmail.com>
2013-12-27 02:59:47 -0500

    First stab at problem 35 of project-euler. Not tested yet.

---
+++ project-euler/problem35.js
@@ -0,0 +1,46 @@
+// The number, 197, is called a circular prime because all rotations of the
+// digits: 197, 971, and 719, are themselves prime.
+// How many circular primes are there below one million?
+
+(function () {
+  'use strict';
+
+  var memoize = {};
+  function isPrime(n) {
+    if (memoize[n]) {
+      return memoize[n];
+    }
+    n = (n < 0) ? -n : n;
+    for (var i = 2; i * i <= n; i++) {
+      if (n % i === 0) {
+        memoize[n] = false;
+        return false;
+      }
+    }
+    memoize[n] = true;
+    return true;
+  }
+
+  function isCircularPrime(n) {
+    var circularPrime = true;
+    var nStr = n + '';
+    for (var i = 0; i < nStr.length; i++) {
+      nStr = nStr.substring(1) + nStr[0];
+      n = parseInt(nStr, 10);
+      if (!isPrime(n)) {
+        circularPrime = false;
+        break;
+      }
+      nStr = n + '';
+    }
+    return circularPrime;
+  }
+
+  var count = 0;
+  for (var i = 2; i <= 100; i++) {
+    if (isCircularPrime(i)) {
+      count++;
+    }
+  }
+  console.log(count);
+})();
