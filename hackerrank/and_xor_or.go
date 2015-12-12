33cd299d6d3dd1de2378f8a42e99a8a17b4a3c5d
Brian Park <yaru22@gmail.com>
2015-12-12 12:34:16 -0500

    HackerRank: Solved performance problem by using bufio.NewReader(os.Stdin).

--- hackerrank/and_xor_or.go
+++ hackerrank/and_xor_or.go
@@ -2,53 +2,45 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func calc(m1, m2 int) int {
 	return ((m1 & m2) ^ (m1 | m2)) & (m1 ^ m2)
 }
 
 func main() {
-	var n, m1, m2, val int
+	var n, max, numMin, val int
+	input := bufio.NewReader(os.Stdin)
+
 	fmt.Scanf("%d\n", &n)
 	arr := make([]int, n)
 	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &arr[i])
-	}
-
-	max := 0
-	for i := 0; i < n-1; i++ {
-		if arr[i] < arr[i+1] {
-			m1, m2 = arr[i], arr[i+1]
-		} else {
-			m1, m2 = arr[i+1], arr[i]
+		fmt.Fscanf(input, "%d", &arr[i])
+		if i > 0 {
+			numMin = arr[i-1]
 		}
-		val = calc(m1, m2)
-		if max < val {
-			max = val
-		}
-		// NOTE: upper bound of calc(m1, m2) is m1 + m2
-		if m1+m2 <= max {
-			continue
-		}
-		for j := i + 2; j < n; j++ {
-			if arr[j] >= m2 {
-				continue
-			} else if arr[j] < m1 {
-				break
-			} else {
-				m1, m2 = m1, arr[j]
-			}
-			val = calc(m1, m2)
+		for j := i - 1; j >= 0; j-- {
+			val = calc(arr[i], numMin)
 			if max < val {
 				max = val
 			}
-			// NOTE: upper bound of calc(m1, m2) is m1 + m2
-			if m1+m2 <= max {
+			if i-j > 5 {
+				break
+			}
+			if numMin <= arr[i] {
+				break
+			}
+			if arr[j] < numMin {
+				numMin = arr[j]
+			}
+			if arr[i]+numMin <= max {
 				break
 			}
 		}
 	}
-
 	fmt.Println(max)
 }
